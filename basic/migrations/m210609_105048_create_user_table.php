3a660f9eef9e3f38cf382f8ba8675a8f3a4d0223
Deg-var <gogpop1229@gmail.com>
2021-06-09 17:09:23 +0500

    init

---
+++ basic/migrations/m210609_105048_create_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use phpDocumentor\Reflection\Types\Integer;
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m210609_105048_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'name'=>$this->string(),
+            'email'=>$this->string()->defaultValue(null),
+            'password'=>$this->string(),
+            'isAdmin'=>$this->integer()->defaultValue(0),
+            'photo'=>$this->string()->defaultValue(null)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
